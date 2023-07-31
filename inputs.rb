2333c38418fd53a9443ee8fbb7b5c21e8d6de718
katfogy <katfogy@gmail.com>
2023-07-31 19:38:10 +0100

    Rubocop error fixed

--- inputs.rb
+++ inputs.rb
@@ -1,13 +1,13 @@
 class Inputs
-    def self.user_input
-      return gets.chomp
-    end
-  
-    def self.user_input_to_i
-      return gets.chomp.to_i
-    end
-  
-    def self.user_permission
-      return gets.chomp.downcase == 'y'
-    end
+  def self.user_input
+    gets.chomp
   end
+
+  def self.user_input_to_i
+    gets.chomp.to_i
+  end
+
+  def self.user_permission
+    gets.chomp.downcase == 'y'
+  end
+end
